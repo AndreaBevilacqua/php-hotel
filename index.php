3494cce4d1a504a088f51023d13472d17ae85217
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-20 19:15:39 +0100

    added hotel table

--- index.php
+++ index.php
@@ -53,6 +53,29 @@ $hotels = [
     <title>Hotel</title>
 </head>
 <body>
-    
+<div class="container mt-4">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center (km)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel): ?>
+                <tr>
+                    <td><?= $hotel['name'] ?></td>
+                    <td><?= $hotel['description'] ?></td>
+                    <td><?= $hotel['parking'] ? 'Yes' : 'No' ?></td>
+                    <td><?= $hotel['vote'] ?></td>
+                    <td><?= $hotel['distance_to_center'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
